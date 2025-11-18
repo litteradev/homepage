e677518ac9a5a11b592818d098d55de358ad5229
tomizawa-r@omega-inc.co <tomizawa-r@omega-inc.co>
2025-11-18 11:23:51 +0900

    ファビコン設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,13 @@ import { Footer } from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "Littera",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "32x32", type: "image/x-icon" },
+      { url: "/favicon.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon.png", sizes: "180x180", type: "image/png" }],
+  },
 };
 
 export default function RootLayout({
